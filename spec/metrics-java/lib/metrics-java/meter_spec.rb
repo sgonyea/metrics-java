ca53ba24e7d6720ddf0162e2ff83ad06fee7d4be
Scott Gonyea <sgonyea@truecar.com>
2012-03-24 19:17:33 -0500

    Added very basic specs for the DSL. Meh.

---
+++ spec/metrics-java/lib/metrics-java/meter_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe MetricsJ::Meter do
+  class MeterTest
+    extend MetricsJ::Meter
+
+    meter :foo
+    meters :baz, 'bar'
+  end
+
+  context "DSL" do
+    describe '.meter' do
+      it "should expose instance methods" do
+        meter_test = MeterTest.new
+
+        meter_test.foo_meter.should be_a com.yammer.metrics.core.Meter
+      end
+    end
+
+    describe '.meters' do
+      it "should expose instance methods" do
+        meter_test = MeterTest.new
+
+        meter_test.baz_meter.should be_a com.yammer.metrics.core.Meter
+        meter_test.bar_meter.should be_a com.yammer.metrics.core.Meter
+      end
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'bundler/setup'
 Bundler.require :default, :development, :test
 
 require 'metrics-java'
-require 'webmock/rspec'
-require 'vcr_setup'
 
 # Pull in the support files
 Dir[ Bundler.root.join("spec/support/**/*.rb") ].each{|f| require f}
